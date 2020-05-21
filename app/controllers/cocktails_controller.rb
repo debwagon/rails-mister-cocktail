2c289a4c26a6ffa98350b6057df91ca203ba3f64
deb wagon <deb.fsmk@gmail.com>
2020-05-21 20:16:16 +0800

    create cocktail form

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,19 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name, :doses, :ingredients))
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+end
